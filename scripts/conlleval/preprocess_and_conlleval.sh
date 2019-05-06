5f8f697f7e9655fb91e1173910c02065c3cdc8f0
IvanSfeir <julien-sfeir-95@hotmail.com>
2019-05-06 11:46:19 +0200

    Upload conlleval folder

---
+++ scripts/conlleval/preprocess_and_conlleval.sh
@@ -0,0 +1,2 @@
+python /home/getalp/sfeirj/scripts/conlleval/preprocess_for_conlleval_no_df.py
+/home/getalp/sfeirj/scripts/conlleval/conlleval.pl < /home/getalp/sfeirj/data/valid_predictions_for_eval | head
